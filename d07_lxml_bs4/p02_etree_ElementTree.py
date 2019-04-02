fb0eef06cd25bea98fd48aeb04ed3437f9471f56
xiajun <1766719700@qq.com>
2019-04-02 10:59:03 +0800

    miller

---
+++ d07_lxml_bs4/p02_etree_ElementTree.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree
+
+tree = xml.etree.ElementTree.ElementTree(file='books.xml')      # 只加载文件
+# print(tree)
+
+# tree2 = xml.etree.ElementTree.ElementTree()       # 第二种方式
+# tree2.parse('books.xml')
+# print(tree2)
+# list的方式遍历：访问：节点名，属性，文本，子结点
+root = tree.getroot()
+for ele_ in root:
+    print(ele_.tag, ele_.attrib, ele_.text)
+    for e_ in  ele_.getchildren():
+        print(ele_.tag, ele_.attrib, ele_.text)
+
+# 使用xpath的方式查找tree：Element支持xpath, find
+
+
